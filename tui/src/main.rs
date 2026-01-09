b05ca3afd6268b073eb8b2cb96b4b99f80d02275
TheLuckMan <167640189+TheLuckMan@users.noreply.github.com>
2026-01-09 15:41:06 +0300

    Update main.rs

--- tui/src/main.rs
+++ tui/src/main.rs
@@ -5,12 +5,8 @@ use remux_core::commands::builtins::register_builtins;
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
-
-    // Создаём реестр команд и регистрируем встроенные команды
     let mut registry = CommandRegistry::new();
     register_builtins(&mut registry);
-
-    // Передаём реестр в App::init
     let mut app = App::init(&args, registry)?;
     app.run()
 }
